457fc26918eb5a1d9833ddb7666142f4b2d87505
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-03-16 19:38:18 +0100

    Commitment of the final LPTHW 43 task

--- ex43.py
+++ ex43.py
@@ -12,9 +12,11 @@ class Scene(object):
 class Engine(object):
 
     def __init__(self, scene_map):
+        # klasyczne przemianowanie na self.
         self.scene_map = scene_map
 
     def play(self):
+        # current scene to scene map przemielone przez opening scene
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
 
@@ -22,45 +24,129 @@ class Engine(object):
             next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
 
-            #be sure to print out the last scene
-            current_scene.enter()
+
+        current_scene.enter()
 
 class Death(Scene):
 
+    quips = [
+        "You died 1",
+        "You died 2",
+        "You died 3",
+        "You died 4",
+    ]
+
     def enter(self):
-        pass
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        exit(1)
 
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""Central Corridor room, enter 'YES' to win, 'NO' to lose and anything else to repeat"""))
+
+        action = input(">  ")
+
+        if action == "YES":
+            print("You pass the trial, young padawan")
+            return 'laser_weapon_armory'
+        elif action == "NO":
+            print("You have failed the trail, you die, mortal!")
+            return 'death'
+
+        else:
+            print ("DOES NOT COMPUTE, MR IRISHMAN")
+            return 'central_corridor'
 
 class LaserWeaponArmory(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        Now we have entered the chapter 2 of the story
+        that I am simplifying greatly
+        the task is to enter a double digit number that is made from numbers 1 and 2 (Yes, I' simplifying the stuff to the max)
+        or to enter 00 to pass, else you die. There are 10 tries so that should be enough"""))
+        # the code is a string made up from a set of 2 random numbers
+        code =f"{randint(1,2)}{randint(1,2)}"
+        guess = input("[KEYPAD]>")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print(" NOT THIS TIME YOUNG LAD")
+            guesses += 1
+            guess = input("[KEYPAD]> ")
+
+        if guess == code:
+            print(dedent("""
+            The code is correct! Bravado
+            you sly slythering slyther
+            new room awaits!
+            """
+            ))
+            return 'the_bridge'
+        else:
+            print(dedent("""
+            something somethin you die something
+            we erace the tripple ""/" with a dedent()
+            have a nice have a nice have a nice day
+            LOOOSER
+            """))
+            return 'death'
 
 class TheBridge(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        Now we are at the brigde
+        maybe there is another game going on
+        just enter whatever the value to get futher
+        """))
+        whatever_value = input(">   ")
+        return "escape_pod"
 
-class EscalePod(Scene):
+class EscapePod(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+            Escape_pod, \n\n\tnothing interesting as I dow't want to continue on
+            playing games
+                They bore me. Maybe I sould play with making a hangman, but I really hate the idea of those study drills
+                I mean, a base of funciton for starts would be nicer
+                Unless waisting time is what we're playing there
+
+
+        """))
+        togo = input("ANYTHING>  ")
+        return 'finished'
+
+class Finished(Scene):
+
+    def enter(self):
+        print("You won! Good job.")
+        YAS = input("< ")
+        return 'finished'
 
 
 class Map(object):
 
-    def __init__(self,start_scene):
-        pass
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
+    }
 
-    def next_scene(self,scene_name):
-        pass
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
